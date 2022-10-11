2d367de37eaf7b1d86fdc8bc0bbeddb2902aaa4c
babin sapkota <babinrules1234@gmail.com>
2022-10-11 20:45:31 +0545

    Styling navigation bar with scss

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
+//Importing Styling Prpoerties
 import "./Home.scss";
+
+//Importing components for pages
+import { NavBar } from "../../components/NavBar/NavBar";
 
 export const Home = (_props:any) => {
     return (
-        <div>
-            Hello From Home Page
+        <div >
+            <NavBar/>
         </div>
     )
 };
